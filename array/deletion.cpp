a31d4f347ed03d78ff17371c8c58d89ef37d7d55
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-08-05 22:40:32 +0530

    delete from end and delete from first added in deletion of array operation

---
+++ array/deletion.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+
+void deletion_at_end(int arr[],int n){
+    n=n-1;
+
+    cout<<"after deleting end element ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void delete_at_first(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    n=n-1;
+    cout<<"after deleting end element ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n ;
+    int arr[100];
+    int index;
+
+    cout<<"no of element present in the array  ";
+    cin >>n;
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+    deletion_at_end(arr,n);
+    delete_at_first(arr,n);
+}
